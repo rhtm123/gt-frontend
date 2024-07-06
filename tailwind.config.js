501c064ace69435096b8d93fa56b9cbea4f714f1
rhtm123 <maurya.iitk@gmail.com>
2024-07-06 18:12:06 +0530

    new theme added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,32 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   daisyui: {
-    themes: ["light", "dark"],
+    themes: [
+      // "light", 
+
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["light"],
+          "primary": "#0048A4",
+          "secondary": "#ff00d3",
+          "accent": "#689ab8",
+        },
+      },
+
+      {
+        dark: {
+          ...require("daisyui/src/theming/themes")["dark"],
+          "primary": "#0048A4",
+          "secondary": "#ff00d3",
+        },
+      },
+      
+      
+      
+      
+      "dark"],
+
+
   },
   plugins: [require("daisyui"), require('@tailwindcss/typography'),],
 };

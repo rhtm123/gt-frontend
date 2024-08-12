ea3f2898b131bccdc31198252e278a0359e7be37
rhtm123 <maurya.iitk@gmail.com>
2024-08-12 19:21:52 +0530

    for loop key added

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -53,7 +53,7 @@ const ProjectsPage = () => {
             {/* Project Cards */}
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
                 {projects.map((project) => (
-                    <ProjectComponent project={project} />
+                    <ProjectComponent key={project.id} project={project} />
                 ))}
             </div>
         </div>
